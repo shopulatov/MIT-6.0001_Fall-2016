896669c03d5e22fab822ed1b550447322bd4762a
Abror Shopulatov <97794364+shopulatov@users.noreply.github.com>
2022-02-04 09:26:13 +0500

    Add files via upload

---
+++ ps0/ps0.py
@@ -0,0 +1,11 @@
+"""
+Created on Thu Feb  3 19:09:05 2022
+
+"""
+import math
+
+x = int(input('Enter number x: '))
+y = int(input('Enter number y: '))
+
+print(f"x**y = {x**y}")
+print(f"logx = {math.log2(x)}")
